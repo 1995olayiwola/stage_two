e89accf1963ec1afd932c11b3e91a7786c2108bd
1995olayiwola <olayiwolarahmon84@gmail.com>
2023-09-14 22:07:35 +0100

    second

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 
 function App() {
@@ -20,7 +25,32 @@ function App() {
 console.log(movie)
   return (
     <div className="App">
-      <h1>{movie.title}</h1>
+      <Container>
+      <Row>
+        <Col sm={12} md={4}>
+        <Card style={{ width: '18rem' }} data-testid="movie-card">
+          {movie?.results?.map((item)=>{
+            return (
+              <div key={item.id}>
+<Card.Img variant="top" src={`https://image.tmdb.org/t/p/w500/${item.poster_path
+}`} data-testid="movie-poster" />
+      <Card.Body>
+        <Card.Title data-testid="movie-title">{item.title}</Card.Title>
+        <Card.Text data-testid="movie-release-date">
+          {item.release_date}
+        </Card.Text>
+        
+      </Card.Body>
+      </div>
+            )
+          })}
+      
+    </Card>
+        </Col>
+        
+      </Row>
+      
+    </Container>
       <div>
       
       </div>
--- src/Components/MovieList.jsx
+++ src/Components/MovieList.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React from 'react';
+const img_url = "https://image.tmdb.org/t/p/w500/"
+const search = 'https://api.themoviedb.org/3/search/movie?query=Jack+Reacher&api_key=9b979c372517159d668828895180769d'
 
-const MovieList = () => {
+const MovieList = ({poster_path,release_date,title,overview
+,popularity
+}) => {
   return (
     <div>MovieList</div>
   )
